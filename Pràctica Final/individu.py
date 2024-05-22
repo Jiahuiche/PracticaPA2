4e30ef1f657abc0e329e8a13db067a97001b311b
JiahaoCheng21 <jiahao.cheng@estudiantat.upc.edu>
2024-05-22 22:13:23 +0200

    Update individu.py

--- Pràctica Final/individu.py
+++ Pràctica Final/individu.py
@@ -1,11 +1,14 @@
 from parella_cromosomes import Cromosomes
 
 class Individu:
-    def __init__(self,):
-        self.__cromosomes__ = Cromosomes()
+    def __init__(self, ID, cromosomes):
+        self.__ID__ = ID
+        self.__cromosomes__ = Cromosomes(cromosomes)
         self.__trets__ = set()
-    def construir_cromosomes (self, lst):
-        self.__cromosomes__.crear(lst)
+    #getters
+    def get_cromosomes (self):
+        self.__cromosomes__.get_cromosomes()
+    #------------------------------
     def informar(self):
         self.__cromosomes__.informar()
         trets = sorted(self.__trets__)
@@ -14,14 +17,18 @@ class Individu:
     def afegir_tret(self, tret):
         self.__trets__.append(tret)
 
-    def interseccio(self, cromosoma2 = None):  #Cromosoma2 serà donde se guarda las intersecciones anteriores.
-        feature_tret = ['-']*len(cromosoma1)
-        if cromosoma2 == None:
-            feature_tret = cromosoma1
-        for i in range(len(cromosoma1)):
-            if cromosoma1[i] == cromosoma2[i]:
-                feature_tret[i] = cromosoma1[i]
-            else:
-                feature_tret[i] = '-'
-        return feature_tret
+    def interseccio(self, intersecció_cromosomes = None):  #intersecció_cromosomes serà donde se guarda las intersecciones anteriores.
+        cromosomes_propi = self.__cromosomes__.get_cromosomes
+        feature_tret = ''
+        if intersecció_cromosomes == None:
+            return cromosomes_propi
+        elif intersecció_cromosomes == '-'*len(cromosomes_propi):
+            return intersecció_cromosomes
+        else:
+            for i in range(len(cromosomes)):
+                if cromosoma1[i] == interseció_cromosomes[i]:
+                    feature_tret += cromosoma1[i]
+                else:
+                    feature_tret += '-'
+            return feature_tret
         
