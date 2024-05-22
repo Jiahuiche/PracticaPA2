f6822046b0ca98948055ecdb2ec9b82f628148a7
Jiahuiche <155244905+Jiahuiche@users.noreply.github.com>
2024-05-22 20:31:21 +0200

    Update conjunt_trets.py

--- Pràctica Final/conjunt_trets.py
+++ Pràctica Final/conjunt_trets.py
@@ -4,13 +4,13 @@ class C_trets:
     def __init__(self):
         self.__dic_trets__ = {tret:(Parella_Cromosomes(), C_individus())}
 
-    def _present(tret):
+    def _present(self, tret):
         for t in self.__dic_trets__ :
             if t == tret:
                 return True
         return False
 
-    def afegir_tret (tret,individu):
+    def afegir_tret (self, tret,individu):
         if self._present(tret):
             print('error')
         else:
@@ -18,6 +18,14 @@ class C_trets:
             self._dic_trets_[tret] = (self.interseccio(), self.get_conjunt_individus())
 
     #def get_conjunt_individus()
+
+    def consulta_tret(self, tret):
+        print(tret)
+        print(self.__dic_trets__[tret][0])
+        for i in range(len(self.__dic_trets__[tret][1]))
+            print(self.__dic_trets__[tret][1][i])
+            
+        
 
     
 
