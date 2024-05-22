0ff76051865645e59d0a8050592670d4846acfd5
Jiahuiche <155244905+Jiahuiche@users.noreply.github.com>
2024-05-22 13:09:40 +0200

    Update parella_cromosomes.py

--- Pràctica Final/parella_cromosomes.py
+++ Pràctica Final/parella_cromosomes.py
@@ -1,10 +1,9 @@
 class Cromosomes:
     def __init__(self):
         self.__cromatide1__ = None
-        self.__cromatide2__ = None
     def get_output(self):
         #Getter que retorn cada cromatida com a string
-        return (self.__cromatide1__, self.__cromatide2__)
+        return self.__cromatide1__
     def crear(self, str):
         #Assigna cada cromatide amb l'estructura de dades del input
         l=len(str)
