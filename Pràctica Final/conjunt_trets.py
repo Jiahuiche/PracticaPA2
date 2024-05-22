0819e6ec81e3d1aecc1979eb10341c3ee6ca1244
Jiahuiche <155244905+Jiahuiche@users.noreply.github.com>
2024-05-22 13:52:07 +0200

    Update conjunt_trets.py

--- Pràctica Final/conjunt_trets.py
+++ Pràctica Final/conjunt_trets.py
@@ -10,16 +10,6 @@ class C_trets:
 
     def get_conjunt_individus()
 
-    def interseccio(cromosoma1,cromosoma2 = None):
-        feature_tret = ['-']*len(cromosoma1)
-        if cromosoma2 == None:
-            feature_tret = cromosoma1
-        for i in range(len(cromosoma1)):
-            if cromosoma1[i] == cromosoma2[i]:
-                feature_tret[i] = cromosoma1[i]
-            else:
-                feature_tret[i] = '-'
-        return feature_tret
     
 
 
