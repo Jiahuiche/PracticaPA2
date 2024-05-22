ece8844005a828aa9c5ea767ab095e6b1ab96b28
Jiahuiche <155244905+Jiahuiche@users.noreply.github.com>
2024-05-22 13:53:12 +0200

    Update individu.py

--- Pràctica Final/individu.py
+++ Pràctica Final/individu.py
@@ -13,4 +13,15 @@ class Individu:
             print(tret)
     def afegir_tret(self, tret):
         self.__trets__.append(tret)
+
+    def interseccio(self, cromosoma2 = None):
+        feature_tret = ['-']*len(cromosoma1)
+        if cromosoma2 == None:
+            feature_tret = cromosoma1
+        for i in range(len(cromosoma1)):
+            if cromosoma1[i] == cromosoma2[i]:
+                feature_tret[i] = cromosoma1[i]
+            else:
+                feature_tret[i] = '-'
+        return feature_tret
         
