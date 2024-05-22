77ac611208d71857117fc04587e5a894b6e65b2e
Jiahuiche <155244905+Jiahuiche@users.noreply.github.com>
2024-05-22 18:41:51 +0200

    Update individu.py

--- Pràctica Final/individu.py
+++ Pràctica Final/individu.py
@@ -2,7 +2,7 @@ from parella_cromosomes import Cromosomes
 
 class Individu:
     def __init__(self,):
-        self.__cromosomes__ = [Cromosomes()]
+        self.__cromosomes__ = Cromosomes()
         self.__trets__ = set()
     def construir_cromosomes (self, lst):
         self.__cromosomes__.creacio(lst)
