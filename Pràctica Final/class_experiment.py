e60245870ffd15f8a253f1b191f2ddb9d93b96f4
Jiahuiche <155244905+Jiahuiche@users.noreply.github.com>
2024-05-22 18:42:57 +0200

    Update class_experiment.py

--- Pràctica Final/class_experiment.py
+++ Pràctica Final/class_experiment.py
@@ -26,7 +26,7 @@ class Experiment:
     def __crear_arbol(self, lst):
         self.__conjunt_individus__.crear_arbol(lst)
 
-    def __construir_cromosomes(self,i,str):
+    def __assignar_cromosomes(self,i,str):
         self._conjunt_individus__.afegir_genoma(i,str)
         
         
