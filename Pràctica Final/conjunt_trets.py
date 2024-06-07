b9e70a2e2b19a4a523c9f6f1fbecfd7612fc0bb4
JiahaoCheng21 <jiahao.cheng@estudiantat.upc.edu>
2024-06-07 18:18:30 +0200

    Update conjunt_trets.py

--- Pràctica Final/conjunt_trets.py
+++ Pràctica Final/conjunt_trets.py
@@ -23,8 +23,9 @@ class C_trets:
     def consulta_tret(self, tret):
         print(tret)
         print(self.__dic_trets__[tret][0])
-        for i in range(len(self.__dic_trets__[tret][1]))
-            print(self.__dic_trets__[tret][1][i])
+        individus=sorted(self._dic_trets_[tret][1])
+        for individu in individus:
+            print(individus.get_nom())
             
         
 
