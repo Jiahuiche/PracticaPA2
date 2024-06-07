583b4b65ebd1d3a7cddd472da8ad3297839767ee
JiahaoCheng21 <jiahao.cheng@estudiantat.upc.edu>
2024-06-07 15:40:01 +0200

    Update conjunt_individus.py

--- Pràctica Final/conjunt_individus.py
+++ Pràctica Final/conjunt_individus.py
@@ -15,9 +15,9 @@ class C_individus:
         if x != marca:
             l = self.llegeix_arbrebinari_int(marca)
             r = self.llegeix_arbrebinari_int(marca)
-            return self.__arbre_genealogic__(x,l,r)
+            return self.__arbre_genealogic__ = ArbreBinari(x,l,r)
         else:
-            return self.__arbre_genealogic__()
+            return self.__arbre_genealogic__=ArbreBinari
 
     def afegir_genoma(self, individu, cromosomes):
         self.__individus__[individu].construir_cromosomes(str)
