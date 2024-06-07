0e4f97d2c691dc690629f86120125987890ffb36
Hui <155244905+Jiahuiche@users.noreply.github.com>
2024-06-07 20:09:57 +0200

    Update individu.py

--- Pràctica Final/individu.py
+++ Pràctica Final/individu.py
@@ -1,22 +1,25 @@
 from parella_cromosomes import Cromosomes
 
 class Individu:
-    def __init__(self, ID, cromosomes):
+    def __init__(self, ID = 0, cromosomes = '0'):
         self.__ID__ = ID
         self.__cromosomes__ = Cromosomes(cromosomes)
         self.__trets__ = set()
+    
     #getters
     def get_cromosomes (self):
         return self.__cromosomes__.get_cromosomes()
 
     def get_ID(self):
         return self.__ID__
+        
     #------------------------------
     def informar(self):
         self.__cromosomes__.informar()
         trets = sorted(self.__trets__)
         for tret in trets:
             print(tret)
+            
     def afegir_tret(self, tret):
         self.__trets__.add(tret)
 
