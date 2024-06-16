6d6ebd2d8ae6510c341bac105b604503cbfab1ef
Hui <155244905+Jiahuiche@users.noreply.github.com>
2024-06-16 19:01:51 +0200

    Update conjunt_individus.py

--- Pràctica Final/conjunt_individus.py
+++ Pràctica Final/conjunt_individus.py
@@ -50,6 +50,6 @@ class C_individus:
         if self.__arbre_genealogic__.buit():
             return self.__arbre_genealogic__.inordre() 
         else:
-            subarbre= _subarbre(self.__arbre_genealogic__)
+            subarbre= _subarbre(self.__arbre_genealogic__._root)
             return subarbre.inordre() if subarbre is not None else None
     
